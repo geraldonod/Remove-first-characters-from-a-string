8b94a71d683c92d549e61a626e05351c647470a3
Gerald <geraldonod@gmail.com>
2024-01-24 22:11:50 +0800

    ask user for string

--- Remove first characters from a string.py
+++ Remove first characters from a string.py
@@ -2,10 +2,13 @@
 def remove_character(string_input, n):
     string_output = string_input[n:]
     return string_output
+
 #ask user for string
+user_input = input("Enter a word: ")
 
 #ask user for number n 
 
 #remove characters based on user input
 
 #print result
+
